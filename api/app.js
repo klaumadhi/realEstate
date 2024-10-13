7ae8ce1bbfa3bb3ce6c9258fea01369a04da5ae7
Klaurent <klaurent.madhi@uni-weimar.com>
2024-10-13 18:26:19 +0200

    testing connection

--- api/app.js
+++ api/app.js
@@ -7,25 +7,18 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
-
 const app = express();
-
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
 app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
-app.listen(process.env.PORT || 9000, () => {
+app.listen(process.env.CLIENT_URL || 9000, () => {
   console.log("Server is running on port 9000");
-});
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello" });
 });
 import { Server } from "socket.io";
 const io = new Server({
@@ -53,17 +46,11 @@ io.on("connection", (socket) => {
   });
   socket.on("sendMessage", ({ receiverId, data }) => {
     const receiver = getUser(receiverId);
-
-    if (receiver) {
-      io.to(receiver.socketId).emit("getMessage", data);
-    } else {
-      console.log(`Receiver with ID ${receiverId} not found or is offline.`);
-    }
+    io.to(receiver.socketId).emit("getMessage", data);
   });
-
   socket.on("disconnect", () => {
     removeUser(socket.id);
     console.log("User disconnected");
   });
 });
-io.listen(4000);
+io.listen("4000");
