c35e80c4016f9d20cef56b4ef2d4ed955009aced
Klaurent <klaurent.madhi@uni-weimar.com>
2024-10-13 17:36:28 +0200

    testing connection

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -65,7 +65,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure: true, // if using HTTPS
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
       })
       .status(200)
