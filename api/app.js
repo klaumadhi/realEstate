ee3a1ed614e7d1c44c5080e0a82b521c2c1949f9
Klaurent <klaurent.madhi@uni-weimar.com>
2024-10-13 11:47:53 +0200

    changes for deplomeent

--- api/app.js
+++ api/app.js
@@ -6,34 +6,46 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
-
 import cookieParser from "cookie-parser";
+import http from "http"; // Import HTTP
 
+// Initialize Express app
 const app = express();
 
+// Middleware setup
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes setup
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
 app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
-app.listen(9000, () => {
-  console.log("Server is running on port 9000");
+
+// Create an HTTP server and attach it to Express
+const server = http.createServer(app);
+
+// Start the server
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
+// Socket.io setup
 import { Server } from "socket.io";
 
-const io = new Server({
+const io = new Server(server, {
+  // Use the same server
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
   },
 });
 
 let onlineUser = [];
+
 const addUser = (userId, socketId) => {
   const userExist = onlineUser.find((user) => user.userId === userId);
   if (!userExist) {
@@ -57,7 +69,9 @@ io.on("connection", (socket) => {
 
   socket.on("sendMessage", ({ receiverId, data }) => {
     const receiver = getUser(receiverId);
-    io.to(receiver.socketId).emit("getMessage", data);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+    }
   });
 
   socket.on("disconnect", () => {
@@ -65,5 +79,3 @@ io.on("connection", (socket) => {
     console.log("User disconnected");
   });
 });
-
-io.listen("4000");
