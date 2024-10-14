945b4cabff844eea83bfa121434e94a2846689e2
Klaurent <klaurent.madhi@uni-weimar.com>
2024-10-15 00:22:59 +0200

    testing connection mobile

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -7,7 +7,14 @@ export const SocketContextProvider = ({children})=>{
     const [socket, setSocket] =useState(null)
  
     useEffect(()=>{
-       setSocket(io("https://realestate-bx27.onrender.com"))
+       setSocket(io("https://realestate-bx27.onrender.com"), {
+        reconnection: true, // Allow reconnection
+        reconnectionAttempts: Infinity, // Retry reconnection indefinitely
+        reconnectionDelay: 1000, // Start retrying after 1 second
+        reconnectionDelayMax: 5000, // Maximum delay between reconnections
+        transports: ["websocket"], // Use WebSocket and disable long polling
+        withCredentials: true, // Send cookies with requests if necessary
+      })
     },[])
     useEffect(()=>{
         currentUser && socket?.emit("newUser", currentUser.id)
