19edbf4b38a1db45bdf38a64694d5e12ecab8ce8
Klaurent <klaurent.madhi@uni-weimar.com>
2024-10-14 23:23:57 +0200

    testing connection

--- api/middleware/verifyToke.js
+++ api/middleware/verifyToke.js
@@ -2,14 +2,18 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  console.log(req.cookies.token); // Check if the token is being received
+  console.log("Token Cookies" + req.cookies); // Check if the token is being received
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
   jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid!" });
+    if (err) {
+      console.error("JWT verification error:", err.message);
+      return res
+        .status(403)
+        .json({ message: "Token is not valid or expired!" });
+    }
     req.userId = payload.id;
-
     next();
   });
 };
