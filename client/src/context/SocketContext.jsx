a47ceae8c8d88a46efc71420f207f4bc3a3eb27c
Klaurent <klaurent.madhi@uni-weimar.com>
2024-10-18 18:36:52 +0200

    testing connection mobile

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,23 +6,16 @@ export const SocketContextProvider = ({children})=>{
     const {currentUser} = useContext(AuthContext)
     const [socket, setSocket] =useState(null)
  
-    useEffect(() => {
-        if (!socket) {
-          const newSocket = io("https://realestate-bx27.onrender.com", {
-            reconnection: true,
-            reconnectionAttempts: Infinity,
-            reconnectionDelay: 500,
-            reconnectionDelayMax: 1000,
-            transports: ["websocket"],
-            withCredentials: true,
-          });
-          setSocket(newSocket);
-      
-          // Clean up on unmount
-          return () => newSocket.close();
-        }
-      }, [socket]);
-      
+    useEffect(()=>{
+       setSocket(io("https://realestate-bx27.onrender.com"), {
+        reconnection: true, // Allow reconnection
+        reconnectionAttempts: Infinity, // Retry reconnection indefinitely
+        reconnectionDelay: 500, // Start retrying after 1 second
+        reconnectionDelayMax: 1000, // Maximum delay between reconnections
+        transports: ["websocket"], // Use WebSocket and disable long polling
+        withCredentials: true, // Send cookies with requests if necessary
+      })
+    },[])
     useEffect(()=>{
         currentUser && socket?.emit("newUser", currentUser.id)
     },[currentUser,socket])
