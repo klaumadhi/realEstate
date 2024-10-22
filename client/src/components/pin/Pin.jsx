b275d4584704e7312c928770da5c1f0a544b3c4c
Klaurent Madhi <120852517+klaumadhi@users.noreply.github.com>
2024-10-22 19:58:59 +0200

    Update Pin.jsx

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,13 +1,30 @@
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
 
+// Import the default Leaflet icons manually
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Create a default icon object
+const defaultIcon = L.icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIconRetina,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 function Pin({ item }) {
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]} icon={defaultIcon}>
       <Popup>
         <div className="popupContainer">
-        <img src={item.images[0]} alt="" />
+          <img src={item.images[0]} alt="" />
           <div className="textContainer">
             <Link to={`/${item.id}`}>{item.title}</Link>
             <span>{item.bedroom} bedroom</span>
